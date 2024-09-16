b5ab23fb2c1eaa4b5f2a382aa521aae3ca8f3596
tarun <ahirwar.tarun@thinksys.com>
2024-09-16 16:31:37 +0530

    added partial prerendering where the static content is served during the build time and the dynamic content is loaded asynchronously into the holes after the static shell is searved.

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 
 const nextConfig = {
   experimental: {
-    appDir: true, // Enable the app directory
+    appDir: true, // Enable the app directory,
+    ppr: 'incremental', // 'incremental value allows you to adopt partial prerendering for specific route.
   },
 };
 
